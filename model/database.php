f0996269d42ee58b111f5a439fe1e31087ffd65b
leocr11 <leonardoplc1@hotmail.com>
2019-03-27 12:32:28 -0500

    second commit

--- model/database.php
+++ model/database.php
@@ -6,7 +6,7 @@ class Database
 	public static function StartUp()
 
 	{
-		$pdo = new PDO('mysql:host=localhost;dbname=cliente;charset=utf8','root','');
+		$pdo = new PDO('mysql:host=localhost;dbname=cliente;charset=utf8','root','MH184dS2NZShL7');
 		$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $pdo;
 	}
